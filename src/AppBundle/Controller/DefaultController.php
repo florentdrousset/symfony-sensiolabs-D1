25019c678856c79b63eae6f5dd7688c4b2147239
Florent Drousset <florent.drousset@gmail.com>
2019-08-12 12:11:47 +0200

    premieres pages Symfony dans le DefaultController

---
+++ src/AppBundle/Controller/DefaultController.php
@@ -0,0 +1,50 @@
+<?php
+
+namespace AppBundle\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\Routing\Annotation\Route;
+
+class DefaultController extends Controller
+{
+    /**
+     * @Route("/", name="homepage")
+     */
+    public function indexAction(Request $request)
+    {
+        // replace this example code with whatever you need
+        /* La méthode 'render' va construire la vue.
+        1er paramètre : lien vers la vue. Celui-ci se trouve dans le dossier app/resources/views
+        2eme paramètre : tableau associatif. 'base_dir' va devenir une variable native dans notre template.
+        */
+        return $this->render('default/index.html.twig', [
+            'base_dir' => realpath($this->getParameter('kernel.project_dir')).DIRECTORY_SEPARATOR,
+        ]);
+    }
+
+    /**
+     * @Route("/about", name="about")
+     */
+     public function aboutAction() {
+        return $this->render('default/about.html.twig', [
+        ]);
+
+     }
+
+    /**
+     * @param string $prenom
+     * @param Request $request
+     * @return \Symfony\Component\HttpFoundation\Response
+     * @Route("/about/{prenom}", name="aboutname")
+     */
+
+    public function aboutController(string $prenom, Request $request) {
+        var_dump($prenom);
+        $prenom = ucfirst($prenom);
+        return $this->render('default/about.html.twig', [
+            'p' => $prenom
+        ]);
+        //Par ce mécanisme, je transmet la valeur de ma variable au template about
+    }
+}
